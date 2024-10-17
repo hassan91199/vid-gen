c4995b7b870a7fbdaa770f90735abf1cab90f74a
Hassan <hassanabbas4745@gmail.com>
2024-10-17 16:20:06 +0500

    fix: create the log folder if it doesn't exists already

--- app/logger.py
+++ app/logger.py
@@ -1,5 +1,11 @@
+import os
 import logging
 
+# Create the .logs directory if it doesn't exist
+if not os.path.exists('.logs'):
+    os.makedirs('.logs')
+
+# Set up logging configuration
 logging.basicConfig(
     filename='.logs/app.log',
     filemode='a',
